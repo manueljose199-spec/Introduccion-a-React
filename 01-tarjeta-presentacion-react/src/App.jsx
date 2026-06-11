58409fc2eae80ee2b16108ddbab735ff7b33a09b
manueljose199-spec <manueljoseemma199@gmail.com>
2026-06-10 19:54:45 -0600

    Configuracion inicial con Vite

---
+++ 01-tarjeta-presentacion-react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PresentationCard from "./components/PresentationCard";
+
+function App() {
+  return (
+    <div className="container">
+      <PresentationCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-tarjeta-presentacion-react/src/components/PresentationCard.jsx
@@ -0,0 +1,25 @@
+function PresentationCard() {
+  return (
+    <div className="card">
+      <img
+        src="https://i.pravatar.cc/300"
+        alt="Perfil"
+      />
+
+      <h1>
+        José Manuel
+        <br />
+        Hernández
+      </h1>
+
+      <h2>Desarrollador Frontend</h2>
+
+      <p>
+        Estudiante apasionado por el desarrollo web con
+        React, JavaScript y Node.js.
+      </p>
+    </div>
+  );
+}
+
+export default PresentationCard;
